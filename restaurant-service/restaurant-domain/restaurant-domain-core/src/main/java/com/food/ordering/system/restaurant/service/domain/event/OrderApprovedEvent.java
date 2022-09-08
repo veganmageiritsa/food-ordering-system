73a93db31f857c9e701302f4eefb7ef2ffe2a470
nikolas leontiou <nle@anixe.io>
2022-09-08 18:00:25 +0300

    Restaurant Service

---
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/event/OrderApprovedEvent.java
@@ -0,0 +1,24 @@
+package com.food.ordering.system.restaurant.service.domain.event;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import com.food.ordering.system.domain.valueobject.RestaurantId;
+import com.food.ordering.system.restaurant.service.domain.entity.OrderApproval;
+
+public class OrderApprovedEvent extends OrderApprovalEvent {
+    
+    public OrderApprovedEvent(
+        OrderApproval orderApproval,
+        RestaurantId restaurantId,
+        List<String> failureMessages,
+        ZonedDateTime createdAt) {
+        super(orderApproval, restaurantId, failureMessages, createdAt);
+    }
+    
+    @Override
+    public void fire() {
+    
+    }
+    
+}
